4be2507a6d378907be36ac0da4adbb596e18c3d1
Sven Vrgoč <newsvenvrgoccode@gmail.com>
2022-09-09 09:13:58 +0200

    preview card

---
+++ 15.article-preview-component-master/script.js
@@ -0,0 +1,9 @@
+const share = document.querySelector('.share-wrap')
+const shareBox = document.querySelector('.share')
+
+
+share.addEventListener('click',(e)=>{
+    shareBox.classList.toggle('show')
+
+})
+    
