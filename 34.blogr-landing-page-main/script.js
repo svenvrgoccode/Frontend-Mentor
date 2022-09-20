48eeaddc26ef7aa5ed1e44242e7a1c155547c312
Sven Vrgoč <newsvenvrgoccode@gmail.com>
2022-09-20 21:38:15 +0200

    blogr finished

--- 34.blogr-landing-page-main/script.js
+++ 34.blogr-landing-page-main/script.js
@@ -1,1 +1,20 @@
+const bar = document.querySelector('.bars')
+const mobMenu = document.querySelector('.mobile-wrap')
+
+
+
+bar.addEventListener('click',()=>{
+    mobMenu.classList.toggle('mobile-wrapActive')
+    if(mobMenu.classList.contains('mobile-wrapActive')){
+      bar.src = 'images/xmark-solid.svg'  
+    }else{
+      bar.src = 'images/icon-hamburger.svg'  
+    }
+})
+
+
+
+
+
+
 
