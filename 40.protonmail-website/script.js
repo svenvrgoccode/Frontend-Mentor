5edeb8f2ad26f67483d3719db3f401bec5ec9955
Sven Vrgoč <newsvenvrgoccode@gmail.com>
2022-10-03 23:55:39 +0200

    dropdown1 finished

--- 40.protonmail-website/script.js
+++ 40.protonmail-website/script.js
@@ -1,1 +1,11 @@
+const dropdown1 = document.querySelector('.dropdown1')
+const dropdown1Link = document.querySelector('.dropdown1-link')
+const angles = document.querySelectorAll('.link-angle')
+
+dropdown1Link.addEventListener('click',(e)=>{
+    dropdown1.classList.toggle('active')
+    angles[0].classList.toggle('active')
+    dropdown1Link.classList.toggle('active')
+    
+})
 
