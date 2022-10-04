84dc1deee9b20140fc675b59b57b5f08f8f06d95
Sven Vrgoč <newsvenvrgoccode@gmail.com>
2022-10-04 10:02:46 +0200

    working on protonmail

--- 40.protonmail-website/script.js
+++ 40.protonmail-website/script.js
@@ -5,9 +5,7 @@ const dropdown2Link = document.querySelector('.dropdown2-link')
 const dropdown3 = document.querySelector('.dropdown3')
 const dropdown3Link = document.querySelector('.dropdown3-link')
 const angles = document.querySelectorAll('.link-angle')
-const menuIcon = document.querySelector('.fa-bars')
-const closeIcon = document.querySelector('.closeIcon')
-const mobMenu = document.querySelector('.mobile-navigation')
+
 
 dropdown1Link.addEventListener('click',(e)=>{
     dropdown1.classList.toggle('active')
@@ -44,10 +42,105 @@ window.onclick = function(e){
     }
 }
 
+const menuIcon = document.querySelector('.fa-bars')
+const closeIcon = document.querySelector('.closeIcon')
+const mobMenu = document.querySelector('.mobile-navigation')
+const mobDropdown1 = document.querySelector('.mobile-dropdown1')
+const mobDropdown2 = document.querySelector('.mobile-dropdown2')
+const mobDropdown3 = document.querySelector('.mobile-dropdown3')
+const mobDropLink1 = document.querySelector('.mob-drop-link1')
+const mobDropLink2 = document.querySelector('.mob-drop-link2')
+const mobDropLink3 = document.querySelector('.mob-drop-link3')
+const backLink = document.querySelectorAll('.back')
+const mobNav = document.querySelector('.mob-nav')
+
 menuIcon.addEventListener('click',(e)=>{
     mobMenu.classList.add('active')
 })
 
 closeIcon.addEventListener('click',(e)=>{
     mobMenu.classList.remove('active')
+})
+
+mobDropLink1.addEventListener('click',(e)=>{
+    mobNav.classList.add('hide')
+    mobDropdown1.classList.add('active')
+})
+mobDropLink2.addEventListener('click',(e)=>{
+    mobNav.classList.add('hide')
+    mobDropdown2.classList.add('active')
+})
+mobDropLink3.addEventListener('click',(e)=>{
+    mobNav.classList.add('hide')
+    mobDropdown3.classList.add('active')
+})
+
+backLink.forEach((back)=>{
+    back.addEventListener('click',(e)=>{
+        mobNav.classList.remove('hide')
+        mobDropdown1.classList.remove('active')
+        mobDropdown2.classList.remove('active')
+        mobDropdown3.classList.remove('active')
+    })
+})
+
+
+// SLIDE +++++++++++++++++++++++++++
+
+
+const slides = document.querySelectorAll('.slide')
+const dataLink = document.querySelectorAll('.data-link')
+
+dataLink.forEach((data)=>{
+    data.addEventListener('mousemove',(e)=>{
+        if(e.target.matches('.data2')){
+            slides[0].classList.remove('active')
+            slides[2].classList.remove('active')
+            slides[3].classList.remove('active')
+            slides[1].classList.add('active')
+
+        }
+        if(e.target.matches('.data1')){
+            slides[1].classList.remove('active')
+            slides[2].classList.remove('active')
+            slides[3].classList.remove('active')
+            slides[0].classList.add('active')
+
+        }
+        if(e.target.matches('.data3')){
+            slides[1].classList.remove('active')
+            slides[0].classList.remove('active')
+            slides[3].classList.remove('active')
+            slides[2].classList.add('active')
+
+        }
+        if(e.target.matches('.data4')){
+            slides[1].classList.remove('active')
+            slides[0].classList.remove('active')
+            slides[2].classList.remove('active')
+            slides[3].classList.add('active')
+
+        }
+      
+    })
+})
+
+
+// TESTIMONIAL
+
+const leftArrow = document.querySelector('.left-wrap')
+const rightArrow = document.querySelector('.right-wrap')
+const slideRow = document.querySelector('.slideRow')
+
+leftArrow.addEventListener('click',(e)=>{
+    slideRow.classList.add('left')
+    rightArrow.classList.remove('hide')
+    leftArrow.classList.add('hide')
+
+})
+rightArrow.addEventListener('click',(e)=>{
+    slideRow.classList.remove('left')
+    leftArrow.classList.remove('hide')
+    rightArrow.classList.add('hide')
+
 })
