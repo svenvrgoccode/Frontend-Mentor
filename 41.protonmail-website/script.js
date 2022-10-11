7c493cb6d811746953662081eef7c3ddbd3e11f3
Sven Vrgoč <newsvenvrgoccode@gmail.com>
2022-10-11 04:26:12 +0200

    proton mail working

--- 41.protonmail-website/script.js
+++ 41.protonmail-website/script.js
@@ -44,6 +44,9 @@ const mobul = document.querySelector('.mobul')
 const mobdropdown =  document.querySelectorAll('.mobdropdown')
 const back = document.querySelectorAll('.back')
 const close = document.querySelector('.close')
+const mobileHeader = document.querySelector('.mob-container')
+const menu = document.querySelector('.fa-bars')
+
 
 back.forEach((back)=>{
     back.addEventListener('click',(e)=>{
@@ -54,6 +57,16 @@ back.forEach((back)=>{
     })
 })
 
+
+menu.addEventListener('click',(e)=>{
+    mobileHeader.classList.add('active')
+})
+
+close.addEventListener('click',(e)=>{
+    mobileHeader.classList.remove('active')
+
+    
+})
 
 
 mobdropdownLink.forEach((moblink)=>{
@@ -78,4 +91,101 @@ mobdropdownLink.forEach((moblink)=>{
 
 
 
+
+//RULE SLIDE
+
+const ruleSlide = document.querySelectorAll('.rule-slide')
+const ruleTab = document.querySelectorAll('.rules .tab')
+
+
+ruleTab.forEach((data)=>{
+    data.addEventListener('mousemove',(e)=>{
+        if(e.target.matches('.data2')){
+            ruleSlide[0].classList.remove('active')
+            ruleSlide[2].classList.remove('active')
+            ruleSlide[3].classList.remove('active')
+            ruleSlide[1].classList.add('active')
+
+        }
+        if(e.target.matches('.data1')){
+            ruleSlide[1].classList.remove('active')
+            ruleSlide[2].classList.remove('active')
+            ruleSlide[3].classList.remove('active')
+            ruleSlide[0].classList.add('active')
+
+        }
+        if(e.target.matches('.data3')){
+            ruleSlide[1].classList.remove('active')
+            ruleSlide[0].classList.remove('active')
+            ruleSlide[3].classList.remove('active')
+            ruleSlide[2].classList.add('active')
+
+        }
+        if(e.target.matches('.data4')){
+            ruleSlide[1].classList.remove('active')
+            ruleSlide[0].classList.remove('active')
+            ruleSlide[2].classList.remove('active')
+            ruleSlide[3].classList.add('active')
+
+        }
+      
+    })
+})
+
+
+// TESTIMONIAL
+
+const arrows = document.querySelectorAll('.testimonial .icon-wrap')
+const testiWrap = document.querySelector('.testimonial-wrap')
+const lines = document.querySelectorAll('.testimonial .line')
+const cards = document.querySelectorAll('.testimonial .card')
+
+
+
+
+arrows.forEach((arrow) => {
+    arrow.addEventListener('click',(e)=>{
+        if(arrow.matches('.right')){
+            testiWrap.classList.add('active')
+            lines[0].classList.remove('active')
+            lines[1].classList.add('active')
+            arrows[1].classList.remove('active')
+            arrows[0].classList.add('active')
+            cards[0].classList.add('active')
+            cards[2].classList.remove('active')
+
+        }
+        if(arrow.matches('.left')){
+            testiWrap.classList.remove('active')
+            lines[1].classList.remove('active')
+            lines[0].classList.add('active')
+            arrows[0].classList.remove('active')
+            arrows[1].classList.add('active')
+            cards[2].classList.add('active')
+            cards[0].classList.remove('active')
+
+
+        }
+    })
+})
+
+
+
+
+const accordLink = document.querySelectorAll('.accord')
+
+accordLink.forEach((link)=>{
+    link.addEventListener('click',(e)=>{
+        const accordUl = e.target.closest(".lg-txt").nextElementSibling
+        const plus = e.target.closest('.lg-txt').querySelector('.foot-plus')
+        accordUl.classList.toggle('active')
+        if(accordUl.classList.contains('active')){
+           plus.src = "../icons/minus-solid.svg"
+
+        }else{
+           plus.src = "../icons/plus-solid.svg"
+
+        }
+    })
+})
 
